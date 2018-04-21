3e928d5f1abb7d31cbf58ca859588fdf8e5b7444
pamarthid <pamarthidurgarao1@gmail.com>
2018-04-21 18:27:28 +0530

    findByMailId changes

--- src/main/java/com/tool/api/user/UserController.java
+++ src/main/java/com/tool/api/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -27,6 +28,15 @@ public class UserController {
 	public UserResponse get() {
 		UserResponse response = new UserResponse();
 		response.setUsers(userService.getAll());
+		response.setSuccess(true);
+		return response;
+	}
+	
+	@GetMapping("/findByMail")
+	@ResponseBody
+	public UserResponse findByMailId(@RequestParam String mail) {
+		UserResponse response = new UserResponse();
+		response.setUser(userService.findByEmail(mail));
 		response.setSuccess(true);
 		return response;
 	}
